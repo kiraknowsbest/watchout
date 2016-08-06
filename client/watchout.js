272b939b9be4580aa3806c66d7f5fb3ba02ed275
kiraknowsbest <kinaharo@gmail.com>
2016-08-05 19:43:45 -0700

    Got player circle on board and moving

--- client/watchout.js
+++ client/watchout.js
@@ -1,9 +1,14 @@
 // initialize enemies
 var enemies = [];
+var player = [{
+  x: 50,
+  y: 50
+}];
+
 for ( var i = 0; i < 11; i++ ) {
   enemies[i] = {
     x: 100 * Math.random(),
-    y: 100 *Math.random()
+    y: 100 * Math.random()
   };
 }
 
@@ -18,6 +23,18 @@ d3.select('svg')
   .attr('r', 2)
   .attr('class', 'enemies')
   .style('fill', 'red');
+
+// player
+d3.select('svg')
+  .selectAll('.player')
+  .data( player )
+  .enter()
+  .append('circle')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', 3)
+  .attr('class', 'player')
+  .style('fill', 'blue');
 
 // update enemies
 var update = function () {
@@ -41,6 +58,31 @@ var timer = function (interval) {
 };
 
 timer( 1000 );
+
+// d3.select('.player').on('mousedown', function(event) {
+//   d3.event.preventDefault;
+//   debugger;
+//   var coordinates = d3.mouse('svg');
+//   d3.select(this)
+//     .attr('cx', function(d) { d.x = coordinate[0]; })
+//     .attr('cy', function(d) { d.y = coordinates[1]; });
+// });
+
+d3.selectAll('.player').on('mousedown', function() {
+  var player = d3.select(this);
+
+  var svg = d3.select('svg')
+    .on('mousemove', mousemove);
+
+  d3.event.preventDefault();
+
+  function mousemove() {
+    player.attr('cx', d3.mouse(svg.node())[0] )
+    .attr('cy', d3.mouse(svg.node())[1]);
+  }
+
+});
+
 
 // var updateEnemies = function() {
 //   d3.select('svg')
