2b5adce1adf0bab1ec8de0ac7b34ae720eb63685
kiraknowsbest <kinaharo@gmail.com>
2016-08-05 17:07:14 -0700

    Got circles drawn on board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,11 @@
-// start slingin' some d3 here.
+var asteroids = [];
+
+for ( var i = 0; i < 11; i++ ) {
+  asteroids[i] = {
+    x: Math.random() * 100,
+    y: Math.random() * 100
+  };
+}
+for ( var i = 0; i < 11; i++ ) {
+  d3.select('svg').selectAll('.enemies').data( asteroids ).enter().append('circle').attr('cx', function(d) { return d.x; }).attr('cy', function(d) { return d.y; }).attr('r', 5).style('fill', 'red');
+}
