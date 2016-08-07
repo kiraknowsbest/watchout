5bd6d851fed06cba985bf6c75937555d5c6c136b
kiraknowsbest <kinaharo@gmail.com>
2016-08-06 18:27:53 -0700

    trying to append images to .enemies

--- client/watchout.js
+++ client/watchout.js
@@ -1,8 +1,8 @@
 // initialize enemies
 var enemies = [];
 var player = [{
-  x: 50,
-  y: 50
+  x: 75,
+  y: 75
 }];
 
 var currentScore = 0;
@@ -11,14 +11,54 @@ var highScore = 0;
 
 for ( var i = 0; i < 11; i++ ) {
   enemies[i] = {
-    x: 100 * Math.random(),
-    y: 100 * Math.random()
+    x: 150 * Math.random(),
+    y: 150 * Math.random()
   };
 }
 
+//appends svg board and patterns to DOM
+var svg = d3.select('.board')
+  .append('svg')
+  .attr('viewBox', '0 0 150 150')
+  .append('defs')
+  .append('pattern')
+  .attr('id', 'asteroid')
+  .attr('x', '0')
+  .attr('y', '0')
+  .attr('height', '10')
+  .attr('width', '10')
+  .style('fill', 'svg:image')
+  .append('svg:image')
+  .attr('x', '0')
+  .attr('y', '0')
+  .attr('height', '4')
+  .attr('xlink:href', 'asteroid.png');
+
+// var defs = svg.append('defs');
+
+// var pattern = svg.append('pattern')
+//   .attr({
+//     'width': 20,
+//     'height': 20,
+//     fill: url('asteroid.png')
+//   });
+
+// svg.selectAll('circle')
+//   .data( enemies )
+//   .enter()
+//   .append('circle')
+//   .attr('cx', function(d) { return d.x; })
+//   .attr('cy', function(d) { return d.y; })
+//   .attr('r', 2)
+//   .attr('class', 'enemies')
+//   .attr('xlink:href', 'asteroid.png')
+//   .attr('width', 4)
+//   .attr('height', 4);
+
+
+
 // appends .enemies to svg
-d3.select('svg')
-  .selectAll('.enemies')
+d3.select('svg').selectAll('circle')
   .data( enemies )
   .enter()
   .append('circle')
@@ -26,11 +66,46 @@ d3.select('svg')
   .attr('cy', function(d) { return d.y; })
   .attr('r', 2)
   .attr('class', 'enemies')
-  .style('fill', 'white')
-  .style('opacity', '.2')
-  .attr('xlink:href', 'asteroid.png')
+  .attr('id', 'asteroid')
   .attr('width', 4)
-  .attr('height', 4);
+  .attr('height', 4)
+  .attr('fill', 'url(#asteroid)');
+  // .attr('x', '-8')
+  // .attr('y', '-8')
+  // .attr('width', '16')
+  // .attr('height', '16');
+
+// var svg = d3.select('svg');
+
+// var defs = d3.select('defs');
+
+// defs.append('svg:pattern')
+//   .attr('id', 'enemy-pattern')
+//   .attr('width', 4)
+//   .attr('height', 4)
+//   .append('svg:circle')
+//   .attr('xlink:href', 'svc:image') //this
+//   .attr('x', 100 * Math.random())
+//   .attr('y', 100 * Math.random())
+//   .attr('width', 4)
+//   .attr('height', 4);
+
+// var svg = d3.select(this.el)
+//   .append('svg')
+//   .attr('width', 750)
+//   .attr('height', 500);
+
+// var defs = svg.append('svg:defs');
+// defs.append('svg:pattern')
+//     .attr('id', 'enemy-pattern')
+//     .attr('width', '4')
+//     .attr('height', '4')
+//     .append('svg:image')
+//     .attr('xlink:href', 'asteroid.png')
+//     .attr('x', 100 * Math.random())
+//     .attr('y', 100 * Math.random())
+//     .attr('width', 4)
+//     .attr('height', 4);
 
 // player element
 d3.select('svg')
@@ -47,13 +122,12 @@ d3.select('svg')
 // update enemies
 var update = function () {
   for ( var i = 0; i < enemies.length; i++ ) {
-    enemies[i].x = Math.random() * 100;
-    enemies[i].y = Math.random() * 100;
+    enemies[i].x = Math.random() * 150;
+    enemies[i].y = Math.random() * 150;
   }
 };
 
 // timer function
-
 var timer = function (interval) {
   setInterval( function() {
     update();
@@ -65,9 +139,6 @@ var timer = function (interval) {
     .attr('cy', function(d) { return d.y; });
   }, interval);
 };
-
-// function func () {}
-// var func = function () {}
 
 // invokes timer
 timer(1000);
@@ -89,7 +160,7 @@ d3.selectAll('.player').on('mousedown', function() {
         currentScore = 0;
       }
     });
-  }, 250 );
+  }, 200 );
 
   var svg = d3.select('svg')
     .on('mousemove', mousemove);
